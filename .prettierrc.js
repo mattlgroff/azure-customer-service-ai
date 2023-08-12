2e7cde95bba331f85319b6a68f6729dc6eaf3acc
Matt Groff <matt@umbrage.com>
2023-08-11 22:16:31 -0400

    Pin version of node

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+// .prettierrc
+module.exports = {
+  semi: true, // Specify if you want to print semicolons at the end of statements
+  singleQuote: true, // If you want to use single quotes
+  arrowParens: 'avoid', // Include parenthesis around a sole arrow function parameter,
+  printWidth: 140,
+  tabWidth: 4,
+  trailingComma: 'es5',
+  bracketSpacing: true
+};
