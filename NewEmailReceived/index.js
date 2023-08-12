07b5bd4160e64377916531631409d439ee25a69c
Matt Groff <matt@umbrage.com>
2023-08-12 00:59:28 -0400

    Sendgrid parse email lets try this

--- NewEmailReceived/index.js
+++ NewEmailReceived/index.js
@@ -4,15 +4,25 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     try {
-        console.log('Sending email...', process.env.SENDGRID_API_KEY)
+        console.log('Sending email...', process.env.SENDGRID_API_KEY);
         sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
+        // Convert buffer to string and parse as JSON
+        const parsedBody = JSON.parse(req.body.toString());
+
+        // Assuming SendGrid sends an array, we'll get the first email event object
+        const emailEvent = parsedBody[0];
+
+        const fromEmail = emailEvent.from.match(/<(.+?)>/)[1]; // Extract email address from string
+        const subject = emailEvent.subject;
+        const body = emailEvent.text;
+
         const msg = {
-            to: 'mattlgroff@gmail.com',
+            to: fromEmail,
             from: 'noreply@groff.dev', // Use the email address or domain you verified above
-            subject: 'We got your message!',
-            text: JSON.stringify(req.body),
-            html: `<strong>${JSON.stringify(req.body)}</strong>`,
+            subject: 'Your customer service request has been processed!',
+            text: `From: ${fromEmail}\nSubject: ${subject}\nBody: ${body}`,
+            html: `<strong>From:</strong> ${fromEmail}<br><strong>Subject:</strong> ${subject}<br><strong>Body:</strong> ${body}`,
         };
 
         await sgMail.send(msg);
@@ -20,8 +30,7 @@ module.exports = async function (context, req) {
         context.res = {
             // status: 200, /* Defaults to 200 */
             body: 'Email sent!',
-        }
-
+        };
     } catch (error) {
         console.error(error);
 
@@ -32,11 +41,6 @@ module.exports = async function (context, req) {
         context.res = {
             status: 500,
             body: 'Error sending email',
-        }
+        };
     }
-
-    // context.res = {
-    //     // status: 200, /* Defaults to 200 */
-    //     body: responseMessage,
-    // };
 };
